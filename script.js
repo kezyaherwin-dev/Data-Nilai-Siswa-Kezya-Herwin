3ca8cf4db6258f5c244e87839dc98ad0cdc05136
kezyaherwin-dev <kezyaputranti@gmail.com>
2026-09-14 12:17:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let riwayat=[];
-function grade(n){if(n>=90)return'A';if(n>=80)return'B';if(n>=70)return'C';if(n>=60)return'D';return'E';}
+function grade(n)
+{if(n>=90)return'A';
+ if(n>=80)return'B';
+ if(n>=70)return'C';
+ if(n>=60)return'D';
+ return'E';}
+
 function status(n){return n>=60?'LULUS':'TIDAK LULUS';}
 function login(){
   let u=document.getElementById('username').value;
@@ -16,6 +22,7 @@ function cekNilaiBaru(){
   if(!nama||isNaN(nilai)||nilai<0||nilai>100){alert("Isi nama & nilai 0-100!");return;}
   let g=grade(nilai); let s=status(nilai);
   let box=document.getElementById('hasil-baru'); box.style.display='block';
+  
   box.innerHTML=`Nama: <b>${nama}</b><br>Nilai: ${nilai} - Grade ${g} - ${s}`;
   riwayat.push({nama,mapel,nilai,g,s});
   let tbody=""; riwayat.forEach(r=>{tbody+=`<tr><td>${r.nama}</td><td>${r.mapel}</td><td>${r.nilai}</td><td>${r.g}</td><td>${r.s}</td></tr>`});
@@ -23,4 +30,5 @@ function cekNilaiBaru(){
   document.getElementById('txt-akhir').innerText=riwayat.length;
   document.getElementById('in-nama').value=""; document.getElementById('in-nilai').value="";
 }
-function logout(){document.querySelectorAll('.page').forEach(x=>x.classList.remove('active'));document.getElementById('page-login').classList.add('active');}
+function logout(){document.querySelectorAll('.page')
+  .forEach(x=>x.classList.remove('active'));document.getElementById('page-login').classList.add('active');}
